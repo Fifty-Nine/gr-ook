dc38810a7b53e58e3af1e41fbf669b05bdf9deb1
Tim Prince <tim.prince@gmail.com>
2017-02-18 15:45:03 -0800

    Add .clang-format.

--- include/ook/decode.h
+++ include/ook/decode.h
@@ -1,56 +1,55 @@
 /* -*- c++ -*- */
-/* 
+/*
  * Copyright 2017 <+YOU OR YOUR COMPANY+>.
- * 
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
  * Boston, MA 02110-1301, USA.
  */
 
-
 #ifndef INCLUDED_OOK_DECODE_H
 #define INCLUDED_OOK_DECODE_H
 
-#include <ook/api.h>
 #include <gnuradio/block.h>
+#include <ook/api.h>
 
-namespace gr {
-  namespace ook {
+namespace gr
+{
+namespace ook
+{
+/*!
+ * \brief <+description of block+>
+ * \ingroup ook
+ *
+ */
+class OOK_API decode : virtual public gr::block
+{
+  public:
+    typedef boost::shared_ptr<decode> sptr;
 
     /*!
-     * \brief <+description of block+>
-     * \ingroup ook
+     * \brief Return a shared_ptr to a new instance of ook::decode.
      *
+     * To avoid accidental use of raw pointers, ook::decode's
+     * constructor is in a private implementation
+     * class. ook::decode::make is the public interface for
+     * creating new instances.
      */
-    class OOK_API decode : virtual public gr::block
-    {
-     public:
-      typedef boost::shared_ptr<decode> sptr;
+    static sptr make();
+};
 
-      /*!
-       * \brief Return a shared_ptr to a new instance of ook::decode.
-       *
-       * To avoid accidental use of raw pointers, ook::decode's
-       * constructor is in a private implementation
-       * class. ook::decode::make is the public interface for
-       * creating new instances.
-       */
-      static sptr make();
-    };
-
-  } // namespace ook
+} // namespace ook
 } // namespace gr
 
 #endif /* INCLUDED_OOK_DECODE_H */
-
--- lib/decode_impl.h
+++ lib/decode_impl.h
@@ -1,17 +1,17 @@
 /* -*- c++ -*- */
-/* 
- * Copyright 2017 <+YOU OR YOUR COMPANY+>.
- * 
+/*
+ * Copyright 2017 Tim Prince.
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
@@ -23,31 +23,31 @@
 
 #include <ook/decode.h>
 
-namespace gr {
-  namespace ook {
+namespace gr
+{
+namespace ook
+{
+class decode_impl : public decode
+{
+  private:
+    struct state;
+    state* state_;
 
-    class decode_impl : public decode
-    {
-     private:
-      struct state;
-      state *state_;
+  public:
+    decode_impl();
+    ~decode_impl();
 
-     public:
-      decode_impl();
-      ~decode_impl();
+    // Where all the action really happens
+    void forecast(int noutput_items, gr_vector_int& ninput_items_required);
 
-      // Where all the action really happens
-      void forecast (int noutput_items, gr_vector_int &ninput_items_required);
+    int general_work(
+      int noutput_items,
+      gr_vector_int& ninput_items,
+      gr_vector_const_void_star& input_items,
+      gr_vector_void_star& output_items);
+};
 
-      int general_work(int noutput_items,
-           gr_vector_int &ninput_items,
-           gr_vector_const_void_star &input_items,
-           gr_vector_void_star &output_items);
-
-    };
-
-  } // namespace ook
+} // namespace ook
 } // namespace gr
 
 #endif /* INCLUDED_OOK_DECODE_IMPL_H */
-
